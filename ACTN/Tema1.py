b3d58c2b8320283c022afe870317091a28910e03
aioanedragos <aioane.costin@yahoo.com>
2020-02-24 13:25:13 +0200

    Merge gitul

--- ACTN/Tema1.py
+++ ACTN/Tema1.py
@@ -1,4 +1,6 @@
 import numpy as np
+from random import randrange
+import time
 
 def Coding(Message,base):
     k = len(str(Message)) + 1
@@ -20,46 +22,61 @@ def Coding(Message,base):
 
     return y
 
-def mul(subsetMessage, i):
-    result = 1
-    for j in subsetMessage:
-        if j != i:
-            # print(j , i)
-            # print(j/(j-i))
-            result = result * (j/(j-i))
-    return result
+def inverse(a,p):
+    return a**(p-2) % p
 
-def fc(subsetMessage,y_copy):
-    arr = [0,1,2]
-    count = 0
-    result = 0
-    while count < 3:
-        ceva = y_copy[subsetMessage[arr[count]]-1] * (mul(subsetMessage,subsetMessage[arr[count]]))
-        result = result + (y_copy[subsetMessage[arr[count]]-1] * (mul(subsetMessage,subsetMessage[arr[count]])))
-        print(ceva)
-        count += 1
-    # print(result)
-    return result
+def mul(subsetA,i,base):
+    prod = 1
+    for j in subsetA:
+        if j != subsetA[i]:
+            prod *= j * inverse(j-subsetA[i],base)
+    return prod
+    
+
+def mul2(x, subsetA, i, base):
+    prod = 1
+    for q in subsetA:
+        if q != subsetA[i]:
+            prod *= ((x - q + base) % base) * inverse(((subsetA[i] - q + base) % base), base)
+    return prod
 
 def Decoding(Message, base):
+    firstSum = 0
+    A = []
+    subsetA = []
     y_copy = Coding(Message, base)
     y = Coding(Message, base)
-    y_copy[1] = 2
+    random_err = randrange(5)
+    y_copy[random_err] = 2
+    # print(y_copy)
+
     mistake = 0
-    subsetMessage = []
+    while mistake < len(y_copy):
+        if y[mistake] != y_copy[mistake]:
+            break
+        mistake += 1
     for i in range(len(y_copy)):
-        if y[i] != y_copy[i]:
-            mistake = i + 1
-    
-    for i in range(len(y)):
-        subsetMessage.append(i + 1)
-    subsetMessage.remove(mistake)
-    # print(subsetMessage)
-    firstResultTry = fc(subsetMessage,y_copy)
-    # if firstResultTry == 0:
-    #     return True
-    # else:
-    #     return False
-    return firstResultTry
+        if i != mistake:
+            A.append(i + 1)
+
+    # print(A)
+    subsetA = [A[0],A[1],A[2]]
+
+    # print(subsetA)
+    firstSum = 0
+    for i in range(len(subsetA)):
+        firstSum += y_copy[subsetA[i] - 1] * mul(subsetA,i,base)
+        # print(y_copy[subsetA[i] - 1])
+    res = firstSum % 11
+
+    # print(res)
+    secondParameters = []
+
+    for x in range(len(subsetA)):
+        ceva =( y_copy[subsetA[x] - 1] * mul2(x + 1, subsetA, x, base))%base
+        print(ceva)
+        # secondParameters.append()
+
 
 print(Decoding(29,11))
+
--- ProiectSmatCard_1/Customer.py
+++ ProiectSmatCard_1/Customer.py
@@ -1,9 +1,24 @@
 import socket,time
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect(("127.0.0.1",1234))
-s.send(b"Mesaj 1")
-time.sleep(1)
-s.send(b"Mesaj 2")
-time.sleep(1)
-s.send(b"exit")
-s.close()
+import Crypto
+from Crypto.PublicKey import RSA
+from Crypto.Cipher import AES
+
+# s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+# s.connect(("127.0.0.1",1234))
+
+new_key = RSA.generate(1024)
+
+public_key = new_key.publickey().exportKey("PEM")
+
+private_key = new_key.exportKey("PEM")
+
+# print(private_key)
+
+public_key_merchant=b""
+with open('PubKM', 'rb') as f:
+        public_key_merchant=f.read()
+print(public_key_merchant)
+
+
+
+# s.close()
