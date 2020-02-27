63de7251cb17dadbb4de50cd8a863068d1d919ee
aioanedragos <aioane.costin@yahoo.com>
2020-02-27 09:44:10 +0200

    Un mic avans la tema

--- ProiectSmatCard_1/Customer.py
+++ ProiectSmatCard_1/Customer.py
@@ -3,34 +3,60 @@ import Crypto
 from Crypto.PublicKey import RSA
 from Crypto.Cipher import AES
 import hashlib
+from Crypto import Random
+from Crypto.Random import random
+import base64
+from Crypto.Cipher import PKCS1_OAEP
+
+BS = 16
+def pad(s):
+    return s + (BS - len(s) % BS) * chr(BS - len(s) % BS)
+def unpad(s):
+    return s[0:-s[-1]] 
+    
+
+class AESCipher:
+    def __init__( self, key ):
+        self.key = key
+
+    def encrypt( self, raw ):
+        raw = pad(raw)
+        iv = Random.new().read( AES.block_size )
+        cipher = AES.new( self.key, AES.MODE_CBC, iv )
+        return base64.b64encode( iv + cipher.encrypt( raw ) )
+
+    def decrypt( self, enc ):
+        enc = base64.b64decode(enc)
+        iv = enc[:16]
+        cipher = AES.new(self.key, AES.MODE_CBC, iv )
+        return unpad(cipher.decrypt( enc[16:] ))
 
 
 
 # s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 # s.connect(("127.0.0.1",1234))
 
-new_key = RSA.generate(1024)
-
-public_key = new_key.publickey().exportKey("PEM")
-
-private_key = new_key.exportKey("PEM")
-
-# print(private_key)
 
 public_key_merchant=b""
+
 with open('PubKM', 'rb') as f:
         public_key_merchant=f.read()
-# print(public_key_merchant)
-
-PrivKC = hashlib.sha256()
+        
+PrivKC=hashlib.sha256()
 PrivKC.update(b"PrivKC")
-aes_key = PrivKC.digest()
+aes_key=PrivKC.digest()
+aes_cipher = AESCipher(aes_key)
 
-# print(aes_key)
 
-# print(public_key_merchant)
-public_key_merchant = RSA.import_key(public_key_merchant)
+public_key_merchant=RSA.importKey(public_key_merchant)
 
-# print(public_key_merchant)
+public_key_merchant = AESCipher(public_key_merchant)
+
+
+aes_key_encryped=public_key_merchant.encrypt(aes_key)
+
+print()
+
+
 
 # s.close()
