97964e4cb3e72530f77dcb815af1e5e8a79475fd
aioanedragos <aioane.costin@yahoo.com>
2020-03-10 18:39:29 +0200

    Am terminat tema 2

--- CN/Tema2/Form1.Designer.cs
+++ CN/Tema2/Form1.Designer.cs
@@ -34,6 +34,12 @@ namespace Tema2
             this.label1 = new System.Windows.Forms.Label();
             this.button2 = new System.Windows.Forms.Button();
             this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // button1
@@ -50,7 +56,7 @@ namespace Tema2
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(289, 42);
+            this.label1.Location = new System.Drawing.Point(348, 39);
             this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(35, 13);
@@ -78,11 +84,74 @@ namespace Tema2
             this.button3.UseVisualStyleBackColor = true;
             this.button3.Click += new System.EventHandler(this.button3_Click);
             // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(27, 252);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(138, 47);
+            this.button4.TabIndex = 4;
+            this.button4.Text = "button4";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(246, 139);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(35, 13);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "label2";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(461, 139);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(35, 13);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "label3";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(213, 212);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(35, 13);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "label4";
+            this.label4.Click += new System.EventHandler(this.label4_Click);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(213, 252);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(35, 13);
+            this.label5.TabIndex = 8;
+            this.label5.Text = "label5";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(213, 286);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(35, 13);
+            this.label6.TabIndex = 9;
+            this.label6.Text = "label6";
+            this.label6.Click += new System.EventHandler(this.label6_Click);
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(600, 366);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.button4);
             this.Controls.Add(this.button3);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.label1);
@@ -106,6 +175,12 @@ namespace Tema2
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Button button2;
         private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
     }
 }
 
--- CN/Tema2/Form1.cs
+++ CN/Tema2/Form1.cs
@@ -9,6 +9,33 @@ namespace Tema2
 {
     public partial class Form1 : Form
     {
+        public double Norma(double[] ceva) {
+            double sum = 0;
+            for (int i = 0; i < ceva.Length; i++)
+                sum += Math.Pow(ceva[i], 2);
+            return sum;
+        }
+
+        public double Norma_1(double[,] ceva) {
+            double norma = 0;
+            int n;
+            if (ceva.Length % 2 == 1)
+                n = ceva.Length / 2 - 1;
+            else
+                n = ceva.Length / 2;
+
+            Console.WriteLine(ceva.Length.ToString());
+            for (int i = 0; i < n; i++) {
+                double sum = 0;
+                for (int j = 0; j < n; j++) {
+                    sum += Math.Abs(ceva[j, i]);
+                }
+                if (sum > norma)
+                    norma = sum;
+            }
+
+            return norma;
+        }
 
         public void calculate(double[,] points)
         {
@@ -63,13 +90,13 @@ namespace Tema2
         }
 
         private void button1_Click(object sender, EventArgs e)
-            {
+        {
             var filePath1 = string.Empty;
 
             using (OpenFileDialog openFileDialog = new OpenFileDialog())
             {
                 openFileDialog.InitialDirectory = "E:\\Facultate\\CN\\Tema2\\Tema2";
-                openFileDialog.Filter = "txt files (*.txt)|*.txt|All files (*.*)|*.*";
+                openFileDialog.Filter = "txt files (.txt)|.txt|All files (.)|*.*";
                 openFileDialog.FilterIndex = 2;
                 openFileDialog.RestoreDirectory = true;
 
@@ -86,23 +113,22 @@ namespace Tema2
             {
                 n++;
             }
-
             double[,] A = new double[n / 2, n / 2];
             double[,] _A = new double[n / 2, n / 2];
 
             int i = 0, j = 0;
             int max = 0;
-            
+
             foreach (var row in sA.Split('\n'))
             {
                 j = 0;
                 int nr = 0;
                 foreach (var col in row.Trim().Split(' '))
                 {
-                    
+
                     if (col.Trim().Contains('.'))
                         nr++;
-                    A[i, j] = double.Parse(col.Trim(),System.Globalization.CultureInfo.InvariantCulture);
+                    A[i, j] = double.Parse(col.Trim(), System.Globalization.CultureInfo.InvariantCulture);
                     _A[i, j] = (double)A[i, j];
                     j++;
                 }
@@ -111,10 +137,13 @@ namespace Tema2
                 i++;
             }
 
+            if (max != 0)
+                n = n - max;
+
             n = n / 2;
-            if(max != 0)
-                n -= max;
-          
+            Console.WriteLine(n.ToString());
+
+
 
             double[,] L = new double[n, n];
             double[,] U = new double[n, n];
@@ -126,7 +155,7 @@ namespace Tema2
                     double sum = 0;
                     for (j = 0; j < i; j++)
                         sum += (L[i, j] * U[j, k]);
-                    U[i, k] = (double)A[i, k] - sum;
+                    U[i, k] = A[i, k] - sum;
                 }
 
                 for (int k = i; k < n; k++)
@@ -138,11 +167,11 @@ namespace Tema2
                         double sum = 0;
                         for (j = 0; j < i; j++)
                             sum += (L[k, j] * U[j, i]);
-                        L[k, i] = ((double)A[k, i] - sum) / U[i, i];
+                        L[k, i] = (A[k, i] - sum) / U[i, i];
                     }
                 }
             }
-            Console.WriteLine("A:");
+            /*Console.WriteLine("A:");
             for (i = 0; i < n; i++)
             {
                 for (j = 0; j < n; j++)
@@ -151,25 +180,35 @@ namespace Tema2
                     
                 }
                 Console.WriteLine();
-            }
-            Console.WriteLine("U:");
+            }*/
+            label1.Text = "Descompunerea LU";
+
+
+            label2.Text = "Matricea U\n";
+            //Console.WriteLine("U:");
 
             for (i = 0; i < n; i++)
             {
                 for (j = 0; j < n; j++)
                 {
+                    label2.Text += U[i, j].ToString() + " ";
                     Console.Write(U[i, j].ToString() + " ");
                 }
+                label2.Text += '\n';
                 Console.WriteLine();
             }
-            Console.WriteLine("L:");
+
+            label3.Text = "Matricea L\n";
+            //Console.WriteLine("L:");
 
             for (i = 0; i < n; i++)
             {
                 for (j = 0; j < n; j++)
                 {
+                    label3.Text += L[i, j].ToString() + " ";
                     Console.Write(L[i, j].ToString() + " ");
                 }
+                label3.Text += '\n';
                 Console.WriteLine();
             }
 
@@ -196,7 +235,7 @@ namespace Tema2
             using (OpenFileDialog openFileDialog = new OpenFileDialog())
             {
                 openFileDialog.InitialDirectory = "E:\\Facultate\\CN\\Tema2\\Tema2";
-                openFileDialog.Filter = "txt files (*.txt)|*.txt|All files (*.*)|*.*";
+                openFileDialog.Filter = "txt files (.txt)|.txt|All files (.)|*.*";
                 openFileDialog.FilterIndex = 2;
                 openFileDialog.RestoreDirectory = true;
 
@@ -213,7 +252,6 @@ namespace Tema2
             {
                 n++;
             }
-
             double[,] A = new double[n / 2, n / 2];
             double[,] _A = new double[n / 2, n / 2];
 
@@ -237,10 +275,10 @@ namespace Tema2
                     max = nr;
                 i++;
             }
-
-            n = n / 2;
             if (max != 0)
-                n -= max;
+                n = n - max;
+            n = n / 2;
+
 
             double[,] L = new double[n, n];
             double[,] U = new double[n, n];
@@ -272,7 +310,13 @@ namespace Tema2
             double determinantU = 1;
             for (i = 0; i < n; i++)
                 determinantU *= U[i, i];
+            label1.Text = "";
+            label2.Text = "";
+            label3.Text = "";
 
+
+            label1.Text = "Determinanatul matricei cu ajutorul descompunerii Lu";
+            label2.Text = determinantU.ToString();
             Console.WriteLine(determinantU.ToString());
         }
 
@@ -283,7 +327,7 @@ namespace Tema2
             using (OpenFileDialog openFileDialog = new OpenFileDialog())
             {
                 openFileDialog.InitialDirectory = "D:\\Facultate Git\\FacultateAn3Sem2\\CN\\Tema2";
-                openFileDialog.Filter = "txt files (*.txt)|*.txt|All files (*.*)|*.*";
+                openFileDialog.Filter = "txt files (.txt)|.txt|All files (.)|*.*";
                 openFileDialog.FilterIndex = 2;
                 openFileDialog.RestoreDirectory = true;
 
@@ -324,22 +368,22 @@ namespace Tema2
                     max = nr;
                 i++;
             }
-
-            n = n / 2;
             if (max != 0)
-                n -= max;
+                n = n - max;
+            n = n / 2;
+
 
             double[,] A_init = new double[n, n];
 
             for (i = 0; i < n; i++)
                 for (j = 0; j < n; j++)
-                    A_init[i, j] = A[i, j]; 
-                    
+                    A_init[i, j] = A[i, j];
+
 
 
             double[,] L = new double[n, n];
             double[,] U = new double[n, n];
-            
+
 
 
             for (i = 0; i < n; i++)
@@ -349,15 +393,15 @@ namespace Tema2
                     double sum = 0;
                     for (j = 0; j < i; j++)
                         sum += (L[i, j] * U[j, k]);
-                    U[i, k] = (double)A[i, k] - sum;
-                    A[i, k] = (double)A_init[i, k] - sum;
+                    U[i, k] = A[i, k] - sum;
+                    A[i, k] = A_init[i, k] - sum;
                 }
 
                 for (int k = i; k < n; k++)
                 {
                     if (i == k)
                         L[i, i] = 1;
-                    if(i!=k)
+                    if (i != k)
                     {
                         double sum = 0;
                         for (j = 0; j < i; j++)
@@ -368,13 +412,16 @@ namespace Tema2
                 }
             }
 
+            label1.Text = "descompunerea LU intr-o singura matrice\n";
 
-/*            for (i = 0; i < n; i++)
+            for (i = 0; i < n; i++)
             {
                 for (j = 0; j < n; j++)
-                    Console.Write(A[i, j].ToString() + " ");
-                Console.WriteLine();
-            }*/
+                    label1.Text += (A[i, j].ToString() + " ");
+                label1.Text += '\n';
+            }
+
+
 
             //==================================
             var filePath2 = string.Empty;
@@ -382,7 +429,7 @@ namespace Tema2
             using (OpenFileDialog openFileDialog = new OpenFileDialog())
             {
                 openFileDialog.InitialDirectory = "D:\\Facultate Git\\FacultateAn3Sem2\\CN\\Tema2";
-                openFileDialog.Filter = "txt files (*.txt)|*.txt|All files (*.*)|*.*";
+                openFileDialog.Filter = "txt files (.txt)|.txt|All files (.)|*.*";
                 openFileDialog.FilterIndex = 2;
                 openFileDialog.RestoreDirectory = true;
 
@@ -393,13 +440,9 @@ namespace Tema2
             }
 
             string sb = System.IO.File.ReadAllText(filePath1);
+            int m;
+            m = n;
 
-            int m = 0;
-            while (sb[m] != '\n')
-            {
-                m++;
-            }
-            m++;
 
             double[,] b = new double[m, 1];
             double[,] b_init = new double[m, 1];
@@ -409,28 +452,34 @@ namespace Tema2
                 b[l, 0] = (double)int.Parse(element);
                 l++;
             }
+            for (i = 0; i < m; i++)
+                Console.WriteLine(b[i, 0]);
 
             for (i = 0; i < m; i++)
                 b_init[i, 0] = b[i, 0];
             double[,] y = new double[m, 1];
 
-            y[0, 0] = b[0, 0];
-            for (i = 1; i < n; i++) {
+            //y[0, 0] = b[0, 0];
+            for (i = 1; i < n; i++)
+            {
                 double sum = 0;
-                for (j = 0; j < n; j++) {
+                for (j = 0; j < n; j++)
+                {
                     if (i > j)
                         sum += A[i, j] * b[j, 0];
                     if (i == j)
                     {
-                        y[j, 0] = (b[j, 0] - sum);
+                        //y[j, 0] = (b[j, 0] - sum);
                         b[j, 0] = (b[j, 0] - sum);
 
                     }
                 }
             }
+            label2.Text = "Matricea Y\n";
             Console.WriteLine("Y:");
             for (i = 0; i < m; i++)
-                Console.WriteLine(y[i, 0]);
+                label2.Text += b[i, 0].ToString() + '\n';
+            //Console.WriteLine(b[i, 0]);
 
             double[,] x = new double[m, 1];
 
@@ -451,12 +500,289 @@ namespace Tema2
             }
 
             Console.WriteLine("X:");
+            label3.Text = "Matricea X\n";
             for (i = 0; i < m; i++)
-                Console.WriteLine(x[i, 0]);
+                label3.Text += x[i, 0].ToString() + '\n';
+            //Console.WriteLine(x[i, 0]);
+            Console.WriteLine("======================");
 
-            
+            double[] vectorRezultate = new double[m];
 
-            
+            for (i = 0; i < m; i++)
+            {
+                double sum = 0;
+                for (j = 0; j < m; j++)
+                {
+                    sum += A_init[i, j] * x[j, 0];
+                }
+
+                vectorRezultate[i] = sum;
+            }
+
+            for (i = 0; i < n; i++)
+                vectorRezultate[i] -= b_init[i, 0];
+
+            label4.Text = "Norma ||A_init * x_Lu - b_init||2 este " + Norma(vectorRezultate).ToString();
+            //Console.WriteLine("Norma este egala cu : " + Norma(vectorRezultate));
+
+            Matrix<double> A_check = DenseMatrix.OfArray(A_init);
+            Matrix<double> b_check = DenseMatrix.OfArray(b_init);
+
+            Console.WriteLine("Inversa matricei A este : ");
+            A_check = A_check.Inverse();
+            for (i = 0; i < n; i++)
+            {
+                for (j = 0; j < n; j++)
+                    Console.Write(A_check[i, j].ToString());
+                Console.WriteLine();
+            }
+
+            var x_lib = A_check.Multiply(b_check);
+
+            Console.WriteLine("Rezultatul ecuatiei Ax=b este:");
+
+            for (i = 0; i < n; i++)
+                Console.WriteLine(x_lib[i, 0].ToString());
+
+
+
+
+            for (i = 0; i < n; i++)
+                vectorRezultate[i] = x[i, 0] - x_lib[i, 0];
+
+            //Console.WriteLine("Norma ||X_LU - X_LIB||2 este : ");
+            label5.Text = "Norma ||X_LU - X_LIB||2 este " + Norma(vectorRezultate).ToString();
+            //Console.WriteLine(Norma(vectorRezultate));
+
+
+            //Console.WriteLine("Ultima norma : ");
+
+            double[] vectorIntermedia = new double[m];
+
+            for (i = 0; i < m; i++)
+            {
+                double sum = 0;
+                for (j = 0; j < m; j++)
+                {
+                    sum += A_check[i, j] * b_init[j, 0];
+                }
+                vectorRezultate[i] = sum;
+            }
+
+            for (i = 0; i < n; i++)
+            {
+                vectorRezultate[i] = x[i, 0] - vectorRezultate[i];
+
+            }
+
+            label6.Text = "Norma ||x_LU - A_invers_lib * b_init este " + Norma(vectorRezultate).ToString();
+
+            Console.WriteLine(Norma(vectorRezultate));
+
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            var filePath1 = string.Empty;
+
+            using (OpenFileDialog openFileDialog = new OpenFileDialog())
+            {
+                openFileDialog.InitialDirectory = "D:\\Facultate Git\\FacultateAn3Sem2\\CN\\Tema2";
+                openFileDialog.Filter = "txt files (.txt)|.txt|All files (.)|*.*";
+                openFileDialog.FilterIndex = 2;
+                openFileDialog.RestoreDirectory = true;
+
+                if (openFileDialog.ShowDialog() == DialogResult.OK)
+                {
+                    filePath1 = openFileDialog.FileName;
+                }
+            }
+
+            string sA = System.IO.File.ReadAllText(filePath1);
+
+            int n = 0;
+            while (sA[n] != '\n')
+            {
+                n++;
+            }
+
+            double[,] A = new double[n / 2, n / 2];
+            double[,] _A = new double[n / 2, n / 2];
+
+            int i = 0, j = 0;
+            int max = 0;
+
+            foreach (var row in sA.Split('\n'))
+            {
+                j = 0;
+                int nr = 0;
+                foreach (var col in row.Trim().Split(' '))
+                {
+
+                    if (col.Trim().Contains('.'))
+                        nr++;
+                    A[i, j] = double.Parse(col.Trim(), System.Globalization.CultureInfo.InvariantCulture);
+                    _A[i, j] = A[i, j];
+                    j++;
+                }
+                if (nr > max)
+                    max = nr;
+                i++;
+            }
+            if (max != 0)
+                n = n - max;
+            n = n / 2;
+
+
+            double[,] A_init = new double[n, n];
+
+            for (i = 0; i < n; i++)
+                for (j = 0; j < n; j++)
+                    A_init[i, j] = A[i, j];
+
+
+
+            double[,] L = new double[n, n];
+            double[,] U = new double[n, n];
+
+
+
+            for (i = 0; i < n; i++)
+            {
+                for (int k = i; k < n; k++)
+                {
+                    double sum = 0;
+                    for (j = 0; j < i; j++)
+                        sum += (L[i, j] * U[j, k]);
+                    U[i, k] = A[i, k] - sum;
+                    A[i, k] = A_init[i, k] - sum;
+                }
+
+                for (int k = i; k < n; k++)
+                {
+                    if (i == k)
+                        L[i, i] = 1;
+                    if (i != k)
+                    {
+                        double sum = 0;
+                        for (j = 0; j < i; j++)
+                            sum += (L[k, j] * U[j, i]);
+                        L[k, i] = (A[k, i] - sum) / U[i, i];
+                        A[k, i] = (A_init[k, i] - sum) / U[i, i];
+                    }
+                }
+            }
+
+
+            /*for (i = 0; i < n; i++)
+            {
+                for (j = 0; j < n; j++)
+                    Console.Write(A[i, j].ToString() + " ");
+                Console.WriteLine();
+            }*/
+
+            double[,] matriceRezultat = new double[n, n];
+            double[] e_j = new double[n];
+
+            label1.Text = "";
+            label2.Text = "";
+            label3.Text = "";
+            label4.Text = "";
+            label5.Text = "";
+            label6.Text = "";
+
+            double[,] matrice_finala_inversa = new double[n, n];
+
+            for (j = 0; j < n; j++)
+            {
+                //intializare vector e_j
+                for (i = 0; i < e_j.Length; i++)
+                    if (i == j)
+                        e_j[i] = 1;
+                    else
+                        e_j[i] = 0;
+                //calcul coloana invers
+
+                for (int k = 1; k < n; k++)
+                {
+                    double sum = 0;
+                    for (int o = 0; o < n; o++)
+                    {
+                        if (k > o)
+                            sum += A[k, o] * e_j[o];
+                        if (k == o)
+                        {
+                            e_j[o] = (e_j[o] - sum);
+
+                        }
+                    }
+                }
+                //Console.WriteLine("ceva" + e_j.Length.ToString());
+
+                //for (int f = 0; f < e_j.Length; f++)
+                //Console.WriteLine(e_j[f].ToString() + " ");
+
+                double[,] x = new double[n, 1];
+                x[n - 1, 0] = e_j[n - 1] / A[n - 1, n - 1];
+
+                for (int k = n - 2; k >= 0; k--)
+                {
+                    double sum = 0;
+                    for (int o = n - 1; o >= 0; o--)
+                    {
+                        if (k < o)
+                            sum += A[k, o] * x[o, 0];
+                        if (k == o)
+                            x[o, 0] = (e_j[o] - sum) / A[k, o];
+
+                    }
+                }
+
+
+                
+
+                label1.Text = "inversul calculat al matricei cu aujtorul descompunerii LU\n";
+                for (int f = 0; f < e_j.Length; f++)
+                {
+                    matrice_finala_inversa[f, j] = x[f, 0];
+                    label2.Text += x[f, 0].ToString() + " ";
+                }
+                //Console.Write(x[f,0] + " ");
+                label2.Text += '\n';
+                Console.WriteLine();
+
+            }
+
+            //==================================
+            Matrix<double> A_check = DenseMatrix.OfArray(A_init);
+            A_check = A_check.Inverse();
+            double[,] A_final = new double[n, n];
+            for (i = 0; i < n; i++)
+            {
+                for (j = 0; j < n; j++)
+                {
+                    A_final[i, j] = matrice_finala_inversa[i, j] - A_check[i, j];
+                } 
+            }
+
+
+            var rezultat = Norma_1(A_final);
+
+            label5.Text = rezultat.ToString();
+
+
+
+        }
+
+
+        private void label6_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label4_Click(object sender, EventArgs e)
+        {
+
         }
     }
 }
