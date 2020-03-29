62f1725100e8f2d14a302b1825598870b792fb62
aioanedragos <aioane.costin@yahoo.com>
2020-03-29 15:56:45 +0300

    Primul subpunct gata

--- ACTN/Tema2.py
+++ ACTN/Tema2.py
@@ -1,12 +1,95 @@
-x = 86
-x = x ** 5
-x = x % 105
-print(105//2)
+def extended_euclidean(a, b): 
+	if a == 0: 
+		return (b, 0, 1) 
+	else: 
+		g, y, x = extended_euclidean(b % a, a) 
+		return (g, x - (b // a) * y, y) 
 
-# genul
-# religia
-# locatia in care se afla
-# varsta
-# anul in care s-a nascut
+# modular inverse driver function 
+def modinv(a, m): 
+	g, x, y = extended_euclidean(a, m) 
+	return x % m 
 
-# setimetele
+# function implementing Chinese remainder theorem 
+# list m contains all the modulii 
+# list x contains the remainders of the equations 
+def crt(m, x): 
+
+	# We run this loop while the list of 
+	# remainders has length greater than 1 
+	while True: 
+		
+		# temp1 will contain the new value 
+		# of A. which is calculated according 
+		# to the equation m1' * m1 * x0 + m0' 
+		# * m0 * x1 
+		temp1 = modinv(m[1],m[0]) * x[0] * m[1] + modinv(m[0],m[1]) * x[1] * m[0] 
+
+		# temp2 contains the value of the modulus 
+		# in the new equation, which will be the 
+		# product of the modulii of the two 
+		# equations that we are combining 
+		temp2 = m[0] * m[1] 
+
+		# we then remove the first two elements 
+		# from the list of remainders, and replace 
+		# it with the remainder value, which will 
+		# be temp1 % temp2 
+		x.remove(x[0]) 
+		x.remove(x[0]) 
+		x = [temp1 % temp2] + x 
+
+		# we then remove the first two values from 
+		# the list of modulii as we no longer require 
+		# them and simply replace them with the new 
+		# modulii that we calculated 
+		m.remove(m[0]) 
+		m.remove(m[0]) 
+		m = [temp2] + m 
+
+		# once the list has only one element left, 
+		# we can break as it will only contain 
+		# the value of our final remainder 
+		if len(x) == 1: 
+			break
+
+	# returns the remainder of the final equation 
+	return x[0] 
+
+
+def tema3_punctul_a():
+    p = 3
+    q = 5
+    r = 7
+    n = 105
+    e = 5
+    d = 29
+    x = 86
+
+    m = []
+    m.append(p)
+    m.append(q)
+    m.append(r)
+
+    y = (x**e) % n
+    print("Y initial este : ", y)
+    # y = (y**d) % n
+    # print("dec(y) este : ", y)
+    
+    x_p = (y % p) ** (d % (p - 1)) % p
+    print("x_p este : ", x_p)
+
+    x_q = (y % q) ** (d % (q - 1)) % q
+    print("x_q este : ", x_q)
+
+    x_r = (y % r) ** (d % (r - 1)) % r
+    print("x_r este : ", x_r)
+
+    x1 = []
+    x1.append(x_p)
+    x1.append(x_q)
+    x1.append(x_r)
+
+    return crt(m,x1)
+
+print(tema3_punctul_a())
