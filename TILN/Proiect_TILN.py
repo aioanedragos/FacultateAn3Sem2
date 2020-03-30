8ccaaa2bf86929c351974f9afdc47516eeb812e2
aioanedragos <aioane.costin@yahoo.com>
2020-03-30 14:05:21 +0300

    gata si cu email-ul

--- TILN/Proiect_TILN.py
+++ TILN/Proiect_TILN.py
@@ -6,3 +6,7 @@ ceva = f.read()
 
 email = re.findall('\S+@\S+', ceva)
 print(email[0])
+
+# adresa https gasita
+urls = re.findall('http[s]?://(?:[a-zA-Z]|[0-9]|[$-_@.&+]|[!*\(\), ]|(?:%[0-9a-fA-F][0-9a-fA-F]))+', ceva)
+print(urls[0])
