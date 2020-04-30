4c147c07546a0d314dc59b566ace7a5536dec52d
aioanedragos <aioane.costin@yahoo.com>
2020-04-30 11:23:30 +0300

    Aproape e gata tema 2

--- CN/Tema5/Partea2.py
+++ CN/Tema5/Partea2.py
@@ -4,6 +4,8 @@ import math
 
 from scipy.sparse import random
 from scipy import stats
+
+from numpy.linalg import inv
 class CustomRandomState(np.random.RandomState):
     def randint(self, k):
         i = np.random.randint(k)
@@ -96,46 +98,153 @@ def metoda_puterii(A):
         
 
         
-# A = np.array([[2,1],[4,2]])
 # metoda_puterii(A)
 
 
-u, s, vh = np.linalg.svd(A, full_matrices=True)
-print("u este :", u)
-print("s este :", s)
-print("v este :", vh)
-nr = 0
-for i in s:
-    if i > 0:
-        nr += 1
-print("Rangul matricei A este :",nr)
 
-maxim = 0
-minim = 999999999999
-for i in s:
-    if i > maxim:
-        maxim = i
-    if i < minim and i > 0:
-        minim = i
+def parte3():
 
-print("Numarul de conditionare al matricei A este :", maxim/minim)
-
-result = vh.dot(s)
-result = result.dot(np.transpose(u))
-nr = 0
-for i in s:
-    nr += 1
+    with open('test.txt', 'r') as f:
+        A_secundar = [[float(num) for num in line.split(',')] for line in f]
 
 
-count = 0
-s_copy = []
-while count != nr:
-    vector = [0] * nr
-    vector[count] = s[count]
-    s_copy.append(vector)
-    count += 1
-s = np.array(s_copy)
-print(s)
-# print("pseudoinversa Moore-Penrose a matricei A este :", result)
+    # print(np.array(l))
+    A_secundar = np.array(A_secundar)
+    # print(len(l[0]))
+    # print(l)
+
+    p = len(A_secundar)
+    m = len(A_secundar[0])
 
 
+
+
+
+
+    u, s, vh = np.linalg.svd(A_secundar, full_matrices=True)
+    print("u este :\n", u)
+    print("s este :", s)
+    print("v este :\n", vh)
+    # ==========================================================
+    nr = 0
+    for i in s:
+        if i > 0:
+            nr += 1
+    print("Rangul matricei A este :",nr)
+
+    maxim = 0
+    minim = 999999999999
+    for i in s:
+        if i > maxim:
+            maxim = i
+        if i < minim and i > 0:
+            minim = i
+    print("Numarul de conditionare al matricei A este :", maxim/minim)
+
+    # result = vh.dot(s)
+    # result = result.dot(np.transpose(u))
+    # nr = 0
+    # for i in s:
+    #     nr += 1
+
+    if p > m:
+        count = 0
+        s_copy = []
+        while count != m:
+            vector = [0] * m
+            vector[count] = s[count]
+            s_copy.append(vector)
+            count += 1
+
+
+        while count != p:
+            for i in s_copy:
+                i.append(0)
+            count += 1
+        s = np.array(s_copy)
+        print(s)
+    else:
+        count = 0
+        s_copy = []
+        while count != p:
+            vector = [0] * p
+            vector[count] = s[count]
+            s_copy.append(vector)
+            count += 1
+
+
+        while count != m:
+            vector = [0] * p
+            s_copy.append(vector)
+            count += 1
+        s = np.array(s_copy)
+        print(s)
+
+
+    A_i = vh.dot(s)
+    A_i = A_i.dot(np.transpose(u))
+    print("prima pseudoinversa Moore-Penrose a matricei A este :\n", A_i)
+
+    s_i = np.array(s)
+
+    # print(s_i)
+
+    if p > m:
+        for i in range(m):
+            s_i[i][i] = 1 / s[i][i]
+    else:
+        for i in range(p):
+            s_i[i][i] = 1 / s[i][i]
+
+    A_i_2 = vh.dot(s_i)
+    A_i_2 = A_i_2.dot(np.transpose(u))
+    print("a doua pseudoinversa Moore-Penrose a matricei A este :\n", A_i)
+
+
+    b = [3]*p
+    x_i = A_i.dot(b)
+
+    print("x_i este : ", x_i)
+
+
+    b = [3]*p
+    x_i_2 = A_i_2.dot(b)
+
+    print("x_i_2 este : ", x_i)
+    print(A_secundar)
+    print(np.transpose(A_secundar))
+
+    A_j = inv(np.transpose(A_secundar).dot(A_secundar)).dot(np.transpose(A_secundar))
+
+    print(" pseudo-invers˘a ın sensul celor mai mici patrate: \n", A_j)
+
+
+    A_final = np.subtract(A_i , A_j)
+    sum = 0
+    for i in range(len(A_final)):
+        for j in range(len(A_final[0])):
+            sum += A_final[i][j]
+    print("Norma este : ", sum)
+
+# parte3()
+
+
+
+import PySimpleGUI as sg
+
+def func(message):
+    print(message)
+
+layout = [[sg.Button('1'), sg.Button('2'), sg.Exit()] ]
+
+window = sg.Window('ORIGINAL').Layout(layout)
+
+while True:             # Event Loop
+    event, values = window.Read()
+    if event in (None, 'Exit'):
+        break
+    if event == '1':
+        metoda_puterii(A)
+    elif event == '2':
+        parte3()
+window.Close()
