9bb3c0744feb8edd62d8a7a19847454d352761fd
aioanedragos <aioane.costin@yahoo.com>
2020-06-06 09:33:54 +0300

    Licenta inceputa

---
+++ Licenta/Licenta/Licenta/App.xaml.cs
@@ -0,0 +1,27 @@
+﻿using Licenta.Views;
+using Xamarin.Forms;
+
+namespace Licenta
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new RegistrationPage();
+        }
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
