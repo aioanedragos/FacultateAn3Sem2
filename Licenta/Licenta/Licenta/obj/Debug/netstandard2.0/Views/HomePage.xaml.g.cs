1078878e6eb6904dc7390ee6a088de656f721493
aioanedragos <aioane.costin@yahoo.com>
2020-06-11 11:34:49 +0300

    Crearea de butoane pentru adaugat si eliminat copii

--- Licenta/Licenta/Licenta/obj/Debug/netstandard2.0/Views/HomePage.xaml.g.cs
+++ Licenta/Licenta/Licenta/obj/Debug/netstandard2.0/Views/HomePage.xaml.g.cs
@@ -17,8 +17,16 @@ namespace Licenta.Views {
     public partial class HomePage : global::Xamarin.Forms.ContentPage {
         
         [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Xamarin.Forms.Build.Tasks.XamlG", "2.0.0.0")]
+        private global::Xamarin.Forms.Entry EntryName;
+        
+        [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Xamarin.Forms.Build.Tasks.XamlG", "2.0.0.0")]
+        private global::Xamarin.Forms.Entry EntryAge;
+        
+        [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Xamarin.Forms.Build.Tasks.XamlG", "2.0.0.0")]
         private void InitializeComponent() {
             global::Xamarin.Forms.Xaml.Extensions.LoadFromXaml(this, typeof(HomePage));
+            EntryName = global::Xamarin.Forms.NameScopeExtensions.FindByName<global::Xamarin.Forms.Entry>(this, "EntryName");
+            EntryAge = global::Xamarin.Forms.NameScopeExtensions.FindByName<global::Xamarin.Forms.Entry>(this, "EntryAge");
         }
     }
 }
